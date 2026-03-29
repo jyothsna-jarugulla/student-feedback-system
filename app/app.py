e71377d5e1b9d4eabe242f2f630bbbdf1f6ea222
jyothsna-jarugulla <jyothsna.j2006@gmail.com>
2026-03-29 16:59:39 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(
     __name__,
-    template_folder="../templates",
-    static_folder="../static"
+    template_folder=os.path.join(BASE_DIR, "../templates"),
+    static_folder=os.path.join(BASE_DIR, "../static")
 )
 
+DB_PATH = os.path.join(BASE_DIR, "feedback.db")
+
 def init_db():
-    conn = sqlite3.connect('feedback.db')
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS feedback (name TEXT, message TEXT)')
     conn.commit()
@@ -19,13 +25,13 @@ def index():
         name = request.form['name']
         message = request.form['message']
 
-        conn = sqlite3.connect('feedback.db')
+        conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
         c.execute("INSERT INTO feedback VALUES (?, ?)", (name, message))
         conn.commit()
         conn.close()
 
-    conn = sqlite3.connect('feedback.db')
+    conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
     c.execute("SELECT * FROM feedback")
     data = c.fetchall()
